56611efc600a7b606f73219d3675ddf7442f2916
EJ-Corp <emgdl2002@gmail.com>
2024-07-03 21:51:47 +1000

    Button Problem is now functional

--- Logi-XH15/Assets/Scripts/ButtonProblem.cs
+++ Logi-XH15/Assets/Scripts/ButtonProblem.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ButtonProblem : MonoBehaviour
 {
+    [SerializeField] private List<InteractButton> buttons;
+    [SerializeField] private InteractButton fixButton;
+
+    //Maybe get rid of this and make it more efficient -> only used to get the flashing warning sign.
+    [SerializeField] private ProblemHandler warningSign;
+    private bool problemActive = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,26 @@ public class ButtonProblem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Debug"))
+        {
+            if(!problemActive)
+            {
+                ActivateProblem();
+            } else 
+            {
+                Debug.Log("Button problem is already active");
+            }
+            
+        }
+    }
+
+    public void ActivateProblem()
+    {
+        int randomButton = Random.RandomRange(0, buttons.Count);
+        fixButton = buttons[randomButton];
+        fixButton.MakeProblem(warningSign);
+        Debug.Log("Problem is the " + buttons[randomButton].name);
+
+        warningSign.StartProblem();
     }
 }
