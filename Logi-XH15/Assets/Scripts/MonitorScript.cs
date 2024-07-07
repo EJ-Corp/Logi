2f94210b2a2cfffdad9bf4204dc01b67f9102219
Ryan Young <ryan@paytvr.net>
2024-07-08 07:31:08 +1000

    fixed monitor ui asset call

--- Logi-XH15/Assets/Scripts/MonitorScript.cs
+++ Logi-XH15/Assets/Scripts/MonitorScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Unity.UI;
+using UnityEngine.UI;
 using TMPro;
 using System.Linq;
 
