9bf360f7b082908cbcd1928ab3f02659832e93ac
Ryanocerou5 <85260279+Ryanocerou5@users.noreply.github.com>
2024-07-08 04:38:25 +1000

    Reorganised scripts

--- Logi-XH15/Assets/Scripts/WinLoseCalculator.cs
+++ Logi-XH15/Assets/Scripts/WinLoseCalculator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLoseCalculator : MonoBehaviour
 {
@@ -18,6 +19,12 @@ public class WinLoseCalculator : MonoBehaviour
         if(timerScript.timeLeft <= 0)
         {
             Debug.Log("Win");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        if(shipIntegrity.currentShipHealth <= 0)
+        {
+            Debug.Log("Lose");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 }
