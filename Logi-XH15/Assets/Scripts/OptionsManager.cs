d285a1efebd66a33d16b648cfcfb2d58fb5c05f0
Ryanocerou5 <85260279+Ryanocerou5@users.noreply.github.com>
2024-07-16 11:41:45 +1000

    Options Menu Sliders Working

--- Logi-XH15/Assets/Scripts/OptionsManager.cs
+++ Logi-XH15/Assets/Scripts/OptionsManager.cs
@@ -24,75 +24,55 @@ public class OptionsManager : MonoBehaviour
 {
     //Reference to Audio Mixer
     [SerializeField] private AudioMixer audioMixer;
-
-    //Current Values
-    [Header("Current Values")]
-    [SerializeField] private float masterVolume;
-    [SerializeField] private float musicVolume;
-    [SerializeField] private float sfxVolume;
-
-    //Max Volumes
-    [Header("Maximum Values")]
-    [SerializeField] private float masterMax;
-    [SerializeField] private float musicMax;
-    [SerializeField] private float sfxMax;
-
-    //Min Volumes
-    [Header("Minimum Values")]
-    [SerializeField] private float masterMin;
-    [SerializeField] private float musicMin;
-    [SerializeField] private float sfxMin;
-
-    //TMP Objects
-    [Header("TMP Objects")]
-    [SerializeField] private TextMeshProUGUI masterVolumeText;
-    [SerializeField] private TextMeshProUGUI musicVolumeText;
-    [SerializeField] private TextMeshProUGUI sfxVolumeText;
-
-    //Slider Objects
-    [SerializeField] private Slider masterVolumeSlider;
-    [SerializeField] private Slider musicVolumeSlider;
-    [SerializeField] private Slider sfxVolumeSlider;
-
     [SerializeField] private OptionsSlider[] optionsSliders;
 
     void Start()
     {
-        for (int i = 1; i < optionsSliders.Length; i++)
+        for (int i = 0; i < optionsSliders.Length - 1; i++)
         {
             OptionsSlider option = optionsSliders[i];
 
             option.sliderObject.minValue = optionsSliders[i].minValue;
-            option.sliderObject.maxValue = optionsSliders[i].minValue;
+            option.sliderObject.maxValue = optionsSliders[i].maxValue;
 
             if(!option.hasBeenChanged)
             {
+                option.currentValue = optionsSliders[i].maxValue;
                 option.sliderObject.value = optionsSliders[i].maxValue;
                 option.textObject.text = "100%";
             }
+
+            option.sliderObject.onValueChanged.AddListener((value) => SetOptionLevel(option, value));
         }
     }
 
-    public void SetVolumeLevel(OptionsSlider option, float value)
+    public void SetOptionLevel(OptionsSlider option, float value)
     {
-        audioMixer.SetFloat(option.sliderName, value);
         option.currentValue = value;
         option.textObject.text = CalculatePercentage(option.currentValue, option.maxValue, option.minValue).ToString() + "%";
+        option.hasBeenChanged = true;
+        if(option.sliderName != "LookSensitivity")
+        {
+            HandleVolumeChange(option.sliderName, value);
+        }
+        else
+        {
+            HandleLookSensitivityChange();
+        }
     }
 
-    public void SetMusicVolumeLevel(float volume)
+    void HandleVolumeChange(string name, float value)
     {
-        audioMixer.SetFloat("MusicVolume", volume);
-        musicVolume = volume;
-        musicVolumeText.text = CalculatePercentage(musicVolume, musicMax, musicMin).ToString() + "%";
+        audioMixer.SetFloat(name, value);
+        if(name == "SFXVolume")
+        {
+            audioMixer.SetFloat("FlareVolume", value);
+        }
     }
 
-    public void SetSFXVolumeLevel(float volume)
+    void HandleLookSensitivityChange()
     {
-        audioMixer.SetFloat("SFXVolume", volume);
-        audioMixer.SetFloat("FlareVolume", volume);
-        sfxVolume = volume;
-        sfxVolumeText.text = CalculatePercentage(sfxVolume, sfxMax, sfxMin).ToString() + "%";
+
     }
 
     public int CalculatePercentage(float volume, float maxValue, float minValue)
